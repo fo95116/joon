11a83ca85b4e80445116f91676b781890ff547d6
fo95116 <fo95116@gmail.com>
2013-12-10 13:15:31 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joon::Application.config.secret_token = 'b3492e8d779a9357f268511829c2c7fee843848aca3459094109e0543e98f9b9984bf4a9dcb8b6510d3496a8ec634e308c0d256e7ecdee1f6baec5008452027f'
