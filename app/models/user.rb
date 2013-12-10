f9c536bd505a0537c2e77c36bec6f3c853294f38
fo95116 <fo95116@gmail.com>
2013-12-10 15:07:57 -0800

    validations and create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ActiveRecord::Base
   # attr_accessible :title, :body
+  attr_accessible :name, :provider, :uid
+
+  has_many :hazards
+  has_many :accidents
+  has_many :votes
+
+  validates :name, presence: true
+  validates :provider, presence: true
+  validates :uid, presence: true, uniqueness: true
+  #will not allow a duplicate uid into the database (unique)
+
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+    end
+  end
 end
