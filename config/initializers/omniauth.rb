eaa03da1709df2fb215b265f47e3bd544aba854e
fo95116 <fo95116@gmail.com>
2013-12-10 15:51:30 -0800

    changed environment variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
  provider :instagram, 'caae8938fffc4e5b9f549695cd1e6933', '3a82993046cb400e8f19a181eba43358'
- provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
+ provider :facebook, 'FACEBOOK_ID', 'FACEBOOK_SECRET'
 
 end
