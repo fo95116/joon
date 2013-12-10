4c6d477bd090c0d1504ab435399f2c0e17aeb1ae
fo95116 <fo95116@gmail.com>
2013-12-10 15:37:21 -0800

    added spas to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Joon::Application.routes.draw do
+
   # resources :sessions, :constraints => { :protocol => "https" }
 
   root to: 'sessions#index'
   # root to: 'home#index'
+
+  resources :spas
 
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
