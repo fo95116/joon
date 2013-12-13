0279d6a4cd36f8987ef9c8fd2918772b8de18b2a
fo95116 <fo95116@gmail.com>
2013-12-13 15:56:06 -0800

    need to work more on the d3 logic to accept more words

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -87,7 +87,7 @@ $(function () {
                               // console.log($info)
                               //=====================================================
                               _.each($info, function(obj) {
-                                  console.log(obj)
+                                  // console.log(obj)
                                   // var $cap = obj['caption']['text']
                                   var $img = obj['images']['low_resolution']['url']
                                   var $comments = obj['comments'].data
@@ -128,13 +128,46 @@ $(function () {
                                         // console.log($text);
                                         $results.push($text);
                                         // console.log($results);
-                                        $('#final').append('<p>' + $text + '</p>')
+                                        // $('#final').append('<p>' + $text + '</p>')
                                       })
                                       // $reults is our final array that will be inserted into d3
-                                      //============================ d3!!!!!!!!!!!!!!!!!=================
-
-                                      //=================================================================
                                       console.log($results)
+                                      //============================ d3!!!!!!!!!!!!!!!!!=================
+                                      var fill = d3.scale.category20();
+
+                                      d3.layout.cloud().size([300, 300])
+                                          .words(['this', 'that', 'very', 'super',
+                                            'ratchet', 'more', 'and', 'moar', 'super freak',
+                                            'money', 'hundreds'].map(function(d) {
+                                            return {text: d, size: 10 + Math.random() * 90};
+                                          }))
+                                          .padding(5)
+                                          .rotate(function() { return ~~(Math.random() * 2) * 90; })
+                                          .font("Helvetica")
+                                          .fontSize(function(d) { return d.size; })
+                                          .on("end", draw)
+                                          .start();
+
+                                      function draw(words) {
+                                        d3.select("body").append("svg")
+                                            .attr("width", 300)
+                                            .attr("height", 300)
+                                          .append("g")
+                                            .attr("transform", "translate(150,150)")
+                                          .selectAll("text")
+                                            .data(words)
+                                          .enter().append("text")
+                                            .style("font-size", function(d) { return d.size + "px"; })
+                                            .style("font-family", "Helvetica")
+                                            .style("fill", function(d, i) { return fill(i); })
+                                            .attr("text-anchor", "middle")
+                                            .attr("transform", function(d) {
+                                              return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                                            })
+                                            .text(function(d) { return d.text; });
+                                      }
+
+                                      //================================================================
                                     } else {
 
                                       $('#final').append('<p>Sorry there are no comments for this image.</p>')
